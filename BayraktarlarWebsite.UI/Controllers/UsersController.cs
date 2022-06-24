164dbec0695211c92bbecb159ddc8353d70bbf1d
Emre YÜCE <emreyuce90@hotmail.com>
2022-06-24 15:42:53 +0300

    password change method implemented

--- BayraktarlarWebsite.UI/Controllers/UsersController.cs
+++ BayraktarlarWebsite.UI/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NToastNotify;
 using System.Threading.Tasks;
 
 namespace BayraktarlarWebsite.UI.Controllers
@@ -12,12 +13,14 @@ namespace BayraktarlarWebsite.UI.Controllers
 
     public class UsersController : Controller
     {
+        private readonly IToastNotification _toastNotification;
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
-        public UsersController(UserManager<User> userManager, SignInManager<User> signInManager)
+        public UsersController(UserManager<User> userManager, SignInManager<User> signInManager, IToastNotification toastNotification)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _toastNotification = toastNotification;
         }
         [HttpGet]
         public IActionResult Add()
@@ -81,6 +84,59 @@ namespace BayraktarlarWebsite.UI.Controllers
             {
                 Users = userList
             });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ChangePassword()
+        {
+            return View(new ChangePasswordViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //ilk olarak sana gelen eski şifreyi kontrol et bakalım eski şifre doğru mu?
+                var user =await _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
+                bool isValid = await _userManager.CheckPasswordAsync(user, model.OldPassword);
+                //eğer kullanıcının girdiği şifre doğru ise
+                if (isValid)
+                {
+                   var result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        //ilk olarak securitstampi değiştir
+                        await _userManager.UpdateSecurityStampAsync(user);
+                        //Kullanıcının oturumunu kapat
+                        await _signInManager.SignOutAsync();
+                        //Kullanıcıya yeni şifreyle tekrar oturum açtır
+                        await _signInManager.PasswordSignInAsync(user,model.NewPassword, true, false);
+                        _toastNotification.AddSuccessToastMessage("Şifre değiştirme işlemi başarılı", new ToastrOptions
+                        {
+                            Title = "İşlem başarılı"
+                        });
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
+                        return View(model);
+                    }
+                }
+                else
+                {
+                    _toastNotification.AddErrorToastMessage("Eski şifrenizi yanlış girdiniz", new ToastrOptions
+                    {
+                        Title = "Hata oluştu"
+                    });
+                    return View(model);
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ BayraktarlarWebsite.UI/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BayraktarlarWebsite.UI.Models
+{
+    public class ChangePasswordViewModel
+    {
+        [DisplayName("Eski Şifreniz :")]
+        [Required(ErrorMessage ="Eski şifreniz alanı gereklidir")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+        [DisplayName("Yeni Şifreniz :")]
+        [Required(ErrorMessage = "Yeni şifreniz alanı gereklidir")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [DisplayName("Yeni Şifreniz Tekrar :")]
+        [Compare("NewPassword",ErrorMessage ="Girdiğiniz şifreler birbiri ile uyuşmuyor")]
+        [Required(ErrorMessage = "Yeni şifreniz tekrar alanı gereklidir")]
+        [DataType(DataType.Password)]
+        public string NewPasswordAgain { get; set; }
+
+    }
+}
